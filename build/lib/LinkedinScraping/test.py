ab3e9ddc5d5a03319842172937796f79f5d85b9b
EdAndradeF <edandradef@gmail.com>
2022-04-04 15:48:08 -0300

    setup

---
+++ build/lib/LinkedinScraping/test.py
@@ -0,0 +1,32 @@
+from LinkedinScraping.ETL import scraping
+
+
+def menu():
+
+    print(
+        '''LinkedBot:
+        0 = Fim 
+        1 = Auto conexao
+        2 = Minhas Candidaturas DataBase
+        '''
+    )
+    return int(input())
+
+
+funcao = menu()
+
+if funcao:
+    bot = scraping.Chrome()
+
+    while funcao:
+
+        if funcao == 1:
+            bot.conect()
+        elif funcao ==2:
+            bot.minhasvagas()
+
+        funcao = menu()
+
+
+
+    bot.bye()
---
+++ readme.md
@@ -0,0 +1,19 @@
+# package_name
+
+Description. 
+	Scraping de dados de vagas e analise da rede Linkedin
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install linkedin_scraping
+```
+
+
+## Author
+EdA
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
